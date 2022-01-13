db16eacf4d88ae9befbb412bc2ec9229437065fd
DESKTOP-SFT9IPP\hongyesol <hong.yesol87@gmail.com>
2022-01-13 21:57:12 +0900

    re upload

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            for (int i = 10; i < 15; i++)
+            {
+                for(int j=0; j<10; j++)
+                {
+                    Console.WriteLine("안내면 술래 가위 바위 보" + i);
+                            }
+                //Console.WriteLine("안내면 술래 가위 바위 보"+i);
+                //Console.WriteLine("for문과 if문 그리고 입력문과 출력문을 모두 쓴다'
+                //문자 하나를 입력 받아서 그 문자가 내 이름과 같다면
+                //출력문이 내 이름 입니다를 10번 반복 for문으로 출력
+                //만약에 다를 경우에는 내이름이 아닙니다를 5번만 출력하는 문");
+            }
+        }
+    }
+}           
+       
--- ConsoleApp5/obj/Debug/netcoreapp3.1/ConsoleApp5.AssemblyInfo.cs
+++ ConsoleApp5/obj/Debug/netcoreapp3.1/ConsoleApp5.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp10")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp10")]
-[assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp10")]
+[assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp5")]
+[assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // MSBuild WriteCodeFragment 클래스에서 생성되었습니다.
