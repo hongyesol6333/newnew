ce78d9049d8dafaaca1e5e8de61856aecd80693f
DESKTOP-SFT9IPP\hongyesol <hong.yesol87@gmail.com>
2022-01-13 21:38:25 +0900

    test test

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
