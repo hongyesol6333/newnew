d5ee9b2f378dd8b35dce9323e7482d88a3667913
DESKTOP-SFT9IPP\hongyesol <hong.yesol87@gmail.com>
2022-01-13 22:11:58 +0900

    newnew

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("사각형의 넓이를 입력하세요");
+            int width = int.Parse(Console.ReadLine());
+            Console.WriteLine("사각형의 넓이를 입력하세요");
+            int hight = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("사각형의 넓이는 : " + width*hight + "입니다.");
+            Console.WriteLine("사각형의 넓이는 : { hight } 입니다.");
+        }
+    }
+}
